0c26875dc8f4730789398e6d0ca69830953847ab
Tatsunori Aoki <ginjiro.135@gmail.com>
2015-12-14 14:32:37 +0900

    Solved problem 025.

---
+++ sh/prob_025.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+a=1; b=1;   # 第1項と2項の生成
+while :; do # フィボナッチ数列の出力
+    c=$(echo "$a + $b" | bc | tr -d '\n\\');
+    echo $a;
+    a=$b;
+    b=$c;
+done \
+    |
+awk 'length($0) == 1000 {print NR; exit}'   # 桁数が1000になる最初の項番号の出力
