7758b09170988d38c7e03666bf8550fc82cb803f
Tatsunori Aoki <ginjiro.135@gmail.com>
2016-01-01 17:34:52 +0900

    Added comment.

--- sh/prob_002.sh
+++ sh/prob_002.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
-a=0; b=1;
+a=1; b=2;
+# 初項の設定
 
 while :; do
     c=$(echo "$a+$b" | bc | tr -d '\n\\');
     echo $a;
     a=$b;
     b=$c;
-done |
-awk '$0 <= 4000000{print} $0 > 4000000{exit}' |
+done \
+| # フィボナッチ数列の生成
+awk '$0 <= 4000000 {print} $0 > 4000000 {exit}' \
+| # 数列の項の値が400万以下の項の出力
 awk '$0 % 2 == 0{sum += $0} END{print sum}'
+  # 偶数の総和の出力
--- sh/prob_004.sh
+++ sh/prob_004.sh
@@ -1,15 +1,24 @@
 #!/bin/sh
 
-for i in `seq 100 999`; do
-    for j in `seq 100 999`; do
-        echo $i $j;
-    done;
-done |
-sed -e 's/ / * /' |
-bc |
-sort -n |
-uniq |
-awk '{split($0, arr, ""); for (i in arr){printf("%d ", arr[i])}; print ""}' |
-awk '$1 == $NF && $2 == $(NF-1) && $3 == $(NF-2){print}' |
-tail -n 1 |
+for i in $(seq 100 999); do
+    for j in $(seq 100 999); do
+        echo $i $j
+    done
+done \
+| # 3桁の数の組合せの出力
+sed -e 's/ / * /' \
+| # 積の算出式の生成
+bc \
+| # 積の出力
+sort -n \
+| # 積のソート
+uniq \
+| # 積のうち、重複したものを除去
+sed -e 's/./& /g' \
+| # 数値を1字毎にスペースで区切り出力
+awk '$1 == $NF && $2 == $(NF-1) && $3 == $(NF-2){print}' \
+| # 回文になっている数値の出力
+tail -n 1 \
+| # 回分のうち、最大値を出力
 tr -d ' '
+  # 区切りスペースの除去
