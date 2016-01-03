1203d3331be7ec8b822113a1fae56bbe2afb5a86
Tatsunori Aoki <ginjiro.135@gmail.com>
2016-01-03 12:00:27 +0900

    Fixed indent.

--- sh/prob_006.sh
+++ sh/prob_006.sh
@@ -9,6 +9,8 @@
     tr ' ' '+' |
     sed -e 's/[1-9][0-9]*/&^2/g' |
     sed -e 's/^/(/' -e 's/$/)/'
-) |
-awk 'BEGIN{RS=""} {print $1,"-",$2}' |
+) \
+|
+awk 'BEGIN{RS=""} {print $1,"-",$2}' \
+|
 bc
