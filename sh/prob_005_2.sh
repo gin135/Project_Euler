beffcbee104315f68b10d46c981baffa45f75419
Tatsunori Aoki <ginjiro.135@gmail.com>
2016-01-04 14:31:32 +0900

    Fixed variable name.

--- sh/prob_005_2.sh
+++ sh/prob_005_2.sh
@@ -4,7 +4,7 @@ seq 1 20 \
 | # 1~20までの整数の出力
 factor \
 | # 素因数分解
-awk '{for(i=2; i<=NF; i++){num[$i]++}; for(fac in num){print fac, num[fac]}; delete num}' \
+awk '{for(i=2; i<=NF; i++){expp[$i]++}; for(fac in expp){print fac, expp[fac]}; delete expp}' \
 | # 1~20の各整数における、素因数の指数の出力
 sort -k 1n,1 -k 2rn,2 \
 | # 1~20の整数を、それぞれ素因数の指数について、降順にソート
