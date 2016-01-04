99677def78bd0aa36e9ac0b7b0d2f54364fb9139
Tatsunori Aoki <ginjiro.135@gmail.com>
2016-01-04 14:29:55 +0900

    Fixed code error.

--- sh/prob_005_2.sh
+++ sh/prob_005_2.sh
@@ -4,9 +4,9 @@ seq 1 20 \
 | # 1~20までの整数の出力
 factor \
 | # 素因数分解
-awk '{for(i=2; i<=NF; i++){num[$i]++}; for(i in num){print i, num[i]}; delete num}' \
+awk '{for(i=2; i<=NF; i++){num[$i]++}; for(fac in num){print fac, num[fac]}; delete num}' \
 | # 1~20の各整数における、素因数の指数の出力
 sort -k 1n,1 -k 2rn,2 \
 | # 1~20の整数を、それぞれ素因数の指数について、降順にソート
-awk 'BEGIN{int=1} $1 != o1 {int *= $1^$2; n=o1} END{print int}'
+awk 'BEGIN {expp=1; o1=0} $1 != o1 {expp *= $1^$2; o1=$1} END {print expp}'
   # 1~20の整数のうち、それぞれについて指数が最大である素因数だけを抽出し、{素因数^指数}を積算したもの(最小公倍数)を出力
