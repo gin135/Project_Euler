7bd9ddb935b22e6b1eb05eb4c0f06fc200bedabb
Tatsunori Aoki <ginjiro.135@gmail.com>
2016-01-14 16:28:16 +0900

    Changed more simple code.

--- sh/prob_012.sh
+++ sh/prob_012.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-seq 1 100000 \
-| # 入力ストリームの生成
+yes 高須クリニック \
+| # yes 高須クリニック
+sed -n '=' \
+| # 自然数の生成
 sed -e 's;[0-9]\{1,\};& * (& + 1) / 2;' \
 | # 三角数の導出式の生成
 bc \
@@ -10,13 +12,11 @@ factor \
 | # 三角数の素因数分解
 awk '{
         sod=1;
-        for(i=2; i<=NF; i++){cod[$i]++};
-        for(i in cod){sod*=(cod[i]+1)};
+        for(i=2; i<=NF; i++){d[$i]++};
+        for(i in d){sod*=(d[i]+1)};
         print $1, sod;
-        for(i in cod){delete cod[i]}
+        for(i in d){delete d[i]}
     }' \
-| # 各三角数の約数の個数を出力(sod: Sum Of Divisor, cod: Count Of Divisor) ##### sodとcodの意味を明らかにしておくこと #####
-awk '$2 > 500 {print $1; exit}' \
-| # 約数の個数が初めて500を超える三角数の出力
-tr -d ':'
-  # 出力の整形
+| # 各三角数について約数の個数を出力
+gawk '500 < $2 {print gensub(/:/, "", "g", $1); exit}'
+  # 約数の個数が初めて500を超える三角数の出力
