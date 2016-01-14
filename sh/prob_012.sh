a05d48e4a26ad00269ab46b135026cba37a4c331
Tatsunori Aoki <ginjiro.135@gmail.com>
2016-01-14 16:44:34 +0900

    Changed more shorter code.

--- sh/prob_012.sh
+++ sh/prob_012.sh
@@ -4,7 +4,7 @@ yes 高須クリニック \
 | # yes 高須クリニック
 sed -n '=' \
 | # 自然数の生成
-sed -e 's;[0-9]\{1,\};& * (& + 1) / 2;' \
+sed -e 's;[0-9]\{1,\};&*(&+1) / 2;' \
 | # 三角数の導出式の生成
 bc \
 | # 三角数の出力
@@ -13,9 +13,8 @@ factor \
 awk '{
         sod=1;
         for(i=2; i<=NF; i++){d[$i]++};
-        for(i in d){sod*=(d[i]+1)};
+        for(n in d){sod*=(d[n]+1); delete d[n]};
         print $1, sod;
-        for(i in d){delete d[i]}
     }' \
 | # 各三角数について約数の個数を出力
 gawk '500 < $2 {print gensub(/:/, "", "g", $1); exit}'
