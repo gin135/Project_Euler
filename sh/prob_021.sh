6b4bc91246e6d1ae5ba101a640409a667a6d463b
Tatsunori Aoki <ginjiro.135@gmail.com>
2016-01-18 23:10:49 +0900

    Fixed indent.

--- sh/prob_021.sh
+++ sh/prob_021.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-seq 1 9999 | awk '{ORS=""; print "@" $0 " "; for(i=1; i*i<=$0; i++){if($0%i == 0){print i " "; if(i != $0/i && $0 != $0/i){print $0/i " "}}}; print "\n"}' | gawk '{sum=0; for(i=2; i<=NF; i++){sum+=$i} print gensub(/@/, "", "g", $1), sum}' | awk '$1 != $2 {print $0 "\n" $2, $1}' | sort -n | uniq -d | awk '{sum+=$1} END{print sum}'
+seq 1 9999 \
+|
+awk 'ORS=""; {print "@" $0 " "; for(i=1; i*i<=$0; i++){if($0%i == 0){print i " "; if(i != $0/i && $0 != $0/i){print $0/i " "}}}; print "\n"}' \
+|
+gawk '{sum=0; for(i=2; i<=NF; i++){sum+=$i}; print gensub(/@/, "", "g", $1), sum}' \
+|
+awk '$1 != $2 {print $0 "\n" $2, $1}' \
+|
+sort -n \
+|
+uniq -d \
+|
+awk '{sum+=$1} END {print sum}'
