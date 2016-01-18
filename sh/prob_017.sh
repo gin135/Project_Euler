b4f30801b2b063b9752dc66ea93564dcb38979da
Tatsunori Aoki <ginjiro.135@gmail.com>
2016-01-18 15:20:55 +0900

    Added comment.

--- sh/prob_017.sh
+++ sh/prob_017.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
 seq 1 1000 \
-|
+| # 1~1000の数字の生成
 sed -e 's;.*;curl -s --retry 5 "http://www.wolframalpha.com/input/?i=&";' \
-|
+| # スクレイピングコマンドの生成
 xargs -i sh -c {} \
-|
-grep 'context.jsonArray.popups.pod_0200.push' \
-|
+| # スクレイピングの実行
+grep 'pod_0200.push' \
+| # 数字の英単語行の抽出
 sed -e 's/[^:]*: "\(.*\)","mI.*/\1/' \
-|
+| # 数字の英単語の抽出
 sed -e '/hundred /aand' \
-|
+| # 文字列"and"の追加
 sed -e 's/^1/one/' \
-|
+| # 数字を英単語に変換
 tr -d '\n -' \
-|
+| # アルファベット以外の文字の除去
 wc -m
+  # 文字数の出力
--- sh/prob_017_2.sh
+++ sh/prob_017_2.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
 seq 1 1000 \
-|
+| # 1~1000の数字の生成
 sed -e 's;.*;curl -s --retry 100 "http://www.wolframalpha.com/input/?i=&";' \
-|
+| # スクレイピングコマンドの生成
 xargs -P 0 -i sh -c {} \
-|
-grep 'context.jsonArray.popups.pod_0200.push' \
-|
+| # Wolfram Alphaさん、すいませんすいませんすいません...
+grep 'pod_0200.push' \
+| # 数字の英単語行の抽出
 sed -e 's/[^:]*: "\(.*\)","mI.*/\1/' \
-|
+| # 数字の英単語の抽出
 sed -e '/hundred /aand' \
-|
+| # 文字列"and"の追加
 sed -e 's/^1/one/' \
-|
+| # 数字を英単語に変換
 tr -d '\n -' \
-|
+| # アルファベット以外の文字の除去
 wc -m
+  # 文字数の出力
