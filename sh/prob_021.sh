e3aac46886f51a524a24d00cf808a61d930d5140
Tatsunori Aoki <ginjiro.135@gmail.com>
2016-01-19 21:39:16 +0900

    Removed redundent code.

--- sh/prob_021.sh
+++ sh/prob_021.sh
@@ -2,9 +2,9 @@
 
 seq 1 9999 \
 | # 10000未満の自然数の生成
-awk 'ORS=""; {print "@" $0 " "; for(i=1; i*i<=$0; i++){if($0%i == 0){print i " "; if(i != $0/i && $0 != $0/i){print $0/i " "}}}; print "\n"}' \
+awk 'ORS=""; {print $0 " "; for(i=1; i*i<=$0; i++){if($0%i == 0){print i " "; if(i != $0/i && $0 != $0/i){print $0/i " "}}}; print "\n"}' \
 | # それぞれの自然数について約数を列挙
-gawk '{sum=0; for(i=2; i<=NF; i++){sum+=$i}; print gensub(/@/, "", "g", $1), sum}' \
+awk '{sum=0; for(i=2; i<=NF; i++){sum+=$i}; print $1, sum}' \
 | # 真の約数の和の出力
 awk '$1 != $2 {print $0 "\n" $2, $1}' \
 | # 友愛数の抽出
