2bf3a48efe7d8b9438591302ac6000e9d181b984
Tatsunori Aoki <ginjiro.135@gmail.com>
2016-02-02 13:55:26 +0900

    Fixed indent.

--- sh/prob_024.sh
+++ sh/prob_024.sh
@@ -10,7 +10,11 @@ awk 'NR != 3; NR == 3 {for(i=NF; 1<=i; i--){p=1; for(j=$i; 1<j; j--){p*=j}; prin
 |
 awk 'NR == 1; NR == 2 {n=$0-1} 3 <= NR {i=1; while($0*i <= n){i++}; print i--; n=n-$0*i}' \
 |
-awk 'NR == 1 {for(i=1; i<=NF; i++){n[i]=$i}} 2 <= NR {ORS=""; for(i=1; i<=$0; i++){$0+=(n[i] == "f") ? 1 : 0} print n[$0] "\n"; n[$0]="f"} END {for(i in n){print (n[i] != "f") ? n[i] : ""}}' \
+awk '
+    NR == 1 {for(i=1; i<=NF; i++){n[i]=$i}}
+    2 <= NR {ORS=""; for(i=1; i<=$0; i++){$0+=(n[i] == "f") ? 1 : 0} print n[$0] "\n"; n[$0]="f"}
+    END {for(i in n){print (n[i] != "f") ? n[i] : ""}}
+    ' \
 |
 xargs \
 |
