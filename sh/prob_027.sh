86d5419ee41e663e4a8917d993451d4a2bc1e366
Tatsunori Aoki <ginjiro.135@gmail.com>
2016-05-07 17:22:26 +0900

    Fixed comment.

--- sh/prob_027.sh
+++ sh/prob_027.sh
@@ -8,11 +8,11 @@ cat <( \
         primes 2 999 | awk '{for(a=-$1+1; a<1000; a++){print a,$1}}'
         # a,bの候補値の生成
     ) \
-|
-awk 'NR==1{for(i=1;i<=NF;i++){prime[$i]=$i}} 1<NR{ORS=""; print $0": "; n=0; while(1){eq=n^2+$1*n+$2; if(prime[eq]==0){print "\n"; next}; print eq" "; n++}}' \
+| # 素数表およびa,bの候補値の出力
+awk 'NR==1{for(i=1; i<=NF; i++){prime[$i]=$i}} 1<NR{ORS=""; print $0": "; n=0; while(1){eq=n^2+$1*n+$2; if(prime[eq]==0){print "\n"; next}; print eq" "; n++}}' \
 | # n=0から始めて、素数を生成できなくなるまで末尾フィールドに出力
 awk '$0=NF" "$0' \
-| # 第一フィールドにフィールド数を付加
+| # フィールド数を第一フィールドとして付加
 sort -k 1,1nr \
 | # 第一フィールド(フィールド数)をキーとして、逆順数値ソート
 head -n 1 \
