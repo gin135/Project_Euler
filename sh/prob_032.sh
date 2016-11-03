18655ff8914b2a75a56e12b23d92ab16216a8b4c
Tatsunori Aoki <ginjiro.135@gmail.com>
2016-11-03 20:03:11 +0900

    Modified problem 032.

--- sh/prob_032.sh
+++ sh/prob_032.sh
@@ -1,1 +1,1 @@
-(loopx <(seq 1 9) <(seq 1234 9876); loopx <(seq 12 98) <(seq 123 987)) | awk '{print $1$2,$1*$2}' | grep -v '0' | grep -v '\(.\).*\1' | sort -k 2,2 | uniq -f 1 | awk '{s+=$2} END{print s}'
+(seq 1 9 |awk '{for(i=1234;i<=9876;i++){print $0,i}}'; seq 12 98 |awk '{for(i=123;i<=987;i++){print $0,i}}') | awk '{print $1$2,$1*$2}' | awk '!index($0, "0") && length($2)==4' | grep -v '\(.\).*\1' | sort -k 2,2 | uniq -f 1 | awk '{s+=$2} END{print s}'
